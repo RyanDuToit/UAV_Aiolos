6d49dcdfe413f580dde413340fc14d6f89057f89
Ryan DuToit <Ryan.DuToit@drake.edu>
2012-06-07 11:05:06 -0500

    Added Xcode Project file and UAV class file

---
+++ UAV_Aiolos/UAV.h
@@ -0,0 +1,7 @@
+//
+//  UAV.h
+//  UAV_Aiolos
+//
+//  Created by Ryan Dutoit on 6/7/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
---
+++ UAV_Aiolos/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  UAV_Aiolos
+//
+//  Created by Ryan Dutoit on 6/7/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

8825c8bb4a421e0a1644aca560b05a87debb3552
Ryan DuToit <Ryan.DuToit@drake.edu>
2012-06-07 11:05:41 -0500

    Stuff?

--- UAV_Aiolos/UAV_Aiolos/UAV.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  UAV.cpp
-//  UAV_Aiolos
-//
-//  Created by Ryan Dutoit on 6/7/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include "UAV.h"
--- UAV_Aiolos/UAV_Aiolos/UAV.h
+++
@@ -1,7 +0,0 @@
-//
-//  UAV.h
-//  UAV_Aiolos
-//
-//  Created by Ryan Dutoit on 6/7/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
